4eb42a32db4acf93c81834f76067364d2b831e84
Stephen Johnston <stephen.j.johnston@gmail.com>
2015-09-23 20:59:46 -0500

    Cleanup and document code
    - Make internal functions private
    - Put a doc string for the package
    - Put in some validation/error checking

--- src/pbkdf2_crack/core.clj
+++ src/pbkdf2_crack/core.clj
@@ -1,20 +1,21 @@
 (ns pbkdf2-crack.core
+  "A tool to recover a lost or forgotten restrictions password on iOS"
  (:require [crypto.password.pbkdf2 :as password]))
 
-(defn check-pbkdf2
+(defn- check-pbkdf2
   "Checks to see if a the pbkdf2 encrypted guess string matches the given 
    encrypted string.  Returns a map that contains the guess and a 
    true/false result."
   [guess hash]
   {:result (password/check guess hash) :value guess })
 
-(defn lazy-list-of-pbkdf2-checks
+(defn- lazy-list-of-pbkdf2-checks
   "For each of the strings in the coll 'guesses', check it against the given
    encrypted hash string. Returns a lazy list of the pbkdf2 checks."
   [hash guesses]
   (map #(check-pbkdf2 % hash) guesses))
 
-(defn zero-padded-list
+(defn- zero-padded-list
   "Given a list of integers, convert them to strings of length four, 
    left-padded with zeros if needed."
   [nums]
@@ -26,23 +27,22 @@
    <iterations>$<salt>$<encrypted password>, where the number of iterations and
    the salt are base64 encoded strings."
   [hash]
-  ;; Thread the numbers thru as the last argument to each of the functions
-  ;; in the chain below. Note that first will ensure that we compute only up to
-  ;; the first positive hit, and no more.
-  (->> (range 1 10000) 
-      (zero-padded-list)
-      (lazy-list-of-pbkdf2-checks hash)
-      (drop-while #(false? (:result %1)))
-      (first))) 
+  (if (or (not (instance? String hash))
+          (not (= (count (re-seq (re-pattern "\\$") hash)) 2)))
+    (throw (IllegalArgumentException. "parameter hash must be a string of the form <base64iterations>$<base64salt>$<encryptedpassword>"))
+    ;; Thread the numbers thru as the last argument to each of the functions
+    ;; in the chain below. Note that first will ensure that we compute only up to
+    ;; the first positive hit, and no more.
+    (->> (range 1 10000) 
+         (zero-padded-list)
+         (lazy-list-of-pbkdf2-checks hash)
+         (drop-while #(false? (:result %1)))
+         (first)))) 
 
 (defn -main
-  "Run pbkdf2-crack.core '<base64iterations>$<base64salt>$<encryptedpassword>'
-   Example: pbkdf2-crack.core 'A+g=$aSbUXg==$M/p4734c8/SOXZnGgZot+BciAW0='
-   Or with Leiningen:  lein run 'A+g=$aSbUXg==$M/p4734c8/SOXZnGgZot+BciAW0='"
+  "Using Leiningen, run lein run '<base64iterations>$<base64salt>$<encryptedpassword>'
+   Example: lein run 'A+g=$aSbUXg==$M/p4734c8/SOXZnGgZot+BciAW0='"
   [& args]
   (if (not (= (count args) 1))
     (println "Usage: lein run '<base64iterations>$<base64salt>$<encryptedpassword>'")
     (println "Crack result: " (brute-force-cracker (first args)))))
-
-
-
